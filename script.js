c4b3d6097b2af54c74c48c5531fe5a5d369dd609
Ishan Shah <ishah8840@gmail.com>
2024-10-22 18:23:47 -0400

    added ambiguous case

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
-const a = document.getElementById("a").value;
-const b = document.getElementById("b").value;
-const c = document.getElementById("c").value;
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('heron').addEventListener('submit', function (event) {
+        event.preventDefault();
 
-const heronFormula = (a,b,c) => {
-    const sa = a*a
-    const sb = b*b
-    const sc = c*c
-    const abc = sa + sb - sc
-    const response = (sqrt((4 * sa * sb) - (abc * abc)))/4
+        const a = parseFloat(document.getElementById("heron-a").value);
+        const b = parseFloat(document.getElementById("heron-b").value);
+        const c = parseFloat(document.getElementById("heron-c").value);
 
-    return response
-}
+        const sa = a * a
+        const sb = b * b
+        const sc = c * c
+        const abc = sa + sb - sc
+        const response = (Math.sqrt((4 * sa * sb) - (abc * abc))) / 4
 
-console.log(heronFormula(a,b,c))
+        document.getElementById("heron-result").value = response.toFixed(2);
+    });
+
+    document.getElementById('ambig').addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const A = parseFloat(document.getElementById("ambig-angle-A").value);
+        const a = parseFloat(document.getElementById("ambig-side-a").value);
+        const b = parseFloat(document.getElementById("ambig-side-b").value);
+
+        const radA = A * (Math.PI / 180);
+        const h = b * Math.sin(radA);
+
+        let response = "";
+
+        if (A < 90) {
+            if (a < h) {
+                response = "No Triangle";
+            } else if (a === h) {
+                response = "Right Triangle";
+            } else if (a > b) {
+                response = "One Triangle";
+            } else if (h < a && a < b) {
+                response = "Two Triangles (ambiguous)";
+            }
+        } else {
+            if (a <= h) {
+                response = "No Triangle";
+            } else if (a > b) {
+                response = "One Triangle";
+            }
+        }
+
+        document.getElementById("ambig-result").value = response;
+    });
+
+    document.getElementById('poly').addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const g = parseFloat(document.getElementById("newt-g").value);
+
+        document.getElementById("newt-result").value = response;
+    });
+});
